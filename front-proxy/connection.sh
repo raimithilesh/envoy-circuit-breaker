610e70736035356379992dfa5ad67a00b86062e0
raimithilesh <59111274+raimithilesh@users.noreply.github.com>
2021-07-01 15:39:28 +0100

    Delete front-proxy directory

--- front-proxy/connection.sh
+++
@@ -1,1 +0,0 @@
-curl -i -H "Content-Type: json" -H "Content-Length: 8000" -X POST -d '{}' http://localhost:8085/service/$1
--- front-proxy/start_envoy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-/usr/bin/dumb-init /bin/sh
-envoy -l debug -c /etc/front-envoy.yaml --service-cluster front-proxy
